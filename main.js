010b2bb088315215f3486a469e6b1fdb702d3504
Mark Whittingham <markwhittingham@Marks-MBP.fios-router.home>
2020-12-16 18:08:21 -0500

    added all

---
+++ main.js
@@ -0,0 +1,95 @@
+stateArr = [{"name": "Alabama", "tieP": "40.78981315", "white": "0.654", "black": "0.265", "hispanic": "0.044", "swing": false, "ECP": "1.67%"}, {"name": "Alaska", "tieP": "273.3939812", "white": "0.6", "black": "0.022", "hispanic": "0.07", "swing": false, "ECP": "0.56%"}, {"name": "Arizona", "tieP": "27.47736998", "white": "0.542", "black": "0.043", "hispanic": "0.318", "swing": true, "ECP": "2.04%"}, {"name": "Arkansas", "tieP": "66.27335639", "white": "0.721", "black": "0.152", "hispanic": "0.078", "swing": false, "ECP": "1.12%"}, {"name": "California", "tieP": "5.061724824", "white": "0.364", "black": "0.053", "hispanic": "0.395", "swing": false, "ECP": "10.22%"}, {"name": "Colorado", "tieP": "34.72984349", "white": "0.678", "black": "0.038", "hispanic": "0.218", "swing": true, "ECP": "1.67%"}, {"name": "Connecticut", "tieP": "56.09646573", "white": "0.657", "black": "0.1", "hispanic": "0.169", "swing": false, "ECP": "1.30%"}, {"name": "Delaware", "tieP": "205.3885746", "white": "0.614", "black": "0.218", "hispanic": "0.097", "swing": false, "ECP": "0.56%"}, {"name": "District of Columbia", "tieP": "283.3868698", "white": "0.367", "black": "0.45", "hispanic": "0.112", "swing": false, "ECP": "0.56%"}, {"name": "Florida", "tieP": "9.311968016", "white": "0.53", "black": "0.15", "hispanic": "0.266", "swing": true, "ECP": "5.39%"}, {"name": "Georgia", "tieP": "18.8369626", "white": "0.52", "black": "0.313", "hispanic": "0.098", "swing": false, "ECP": "2.97%"}, {"name": "Hawaii", "tieP": "141.2557067", "white": "0.204", "black": "0.012", "hispanic": "0.103", "swing": false, "ECP": "0.74%"}, {"name": "Idaho", "tieP": "111.9153472", "white": "0.816", "black": "0.007", "hispanic": "0.129", "swing": false, "ECP": "0.74%"}, {"name": "Illinois", "tieP": "15.78305123", "white": "0.609", "black": "0.136", "hispanic": "0.176", "swing": false, "ECP": "3.72%"}, {"name": "Indiana", "tieP": "29.70788681", "white": "0.786", "black": "0.092", "hispanic": "0.072", "swing": false, "ECP": "2.04%"}, {"name": "Iowa", "tieP": "63.39003572", "white": "0.853", "black": "0.039", "hispanic": "0.063", "swing": true, "ECP": "1.12%"}, {"name": "Kansas", "tieP": "68.65034116", "white": "0.757", "black": "0.055", "hispanic": "0.122", "swing": false, "ECP": "1.12%"}, {"name": "Kentucky", "tieP": "44.76603368", "white": "0.845", "black": "0.08", "hispanic": "0.037", "swing": false, "ECP": "1.49%"}, {"name": "Louisiana", "tieP": "43.02191063", "white": "0.588", "black": "0.319", "hispanic": "0.052", "swing": false, "ECP": "1.49%"}, {"name": "Maine", "tieP": "148.7860546", "white": "0.932", "black": "0.014", "hispanic": "0.017", "swing": false, "ECP": "0.74%"}, {"name": "Maryland", "tieP": "33.08147305", "white": "0.499", "black": "0.296", "hispanic": "0.106", "swing": false, "ECP": "1.86%"}, {"name": "Massachusetts", "tieP": "29.01703489", "white": "0.705", "black": "0.07", "hispanic": "0.124", "swing": false, "ECP": "2.04%"}, {"name": "Michigan", "tieP": "20.02632059", "white": "0.749", "black": "0.134", "hispanic": "0.053", "swing": true, "ECP": "2.97%"}, {"name": "Minnesota", "tieP": "35.46330683", "white": "0.791", "black": "0.064", "hispanic": "0.056", "swing": false, "ECP": "1.86%"}, {"name": "Mississippi", "tieP": "67.20093651", "white": "0.566", "black": "0.376", "hispanic": "0.03", "swing": false, "ECP": "1.12%"}, {"name": "Missouri", "tieP": "32.58694033", "white": "0.794", "black": "0.112", "hispanic": "0.043", "swing": false, "ECP": "1.86%"}, {"name": "Montana", "tieP": "187.1296003", "white": "0.861", "black": "0.006", "hispanic": "0.037", "swing": false, "ECP": "0.56%"}, {"name": "Nebraska", "tieP": "103.3908048", "white": "0.785", "black": "0.048", "hispanic": "0.113", "swing": false, "ECP": "0.93%"}, {"name": "Nevada", "tieP": "64.93177618", "white": "0.478", "black": "0.092", "hispanic": "0.293", "swing": true, "ECP": "1.12%"}, {"name": "New Hampshire", "tieP": "147.0900802", "white": "0.898", "black": "0.013", "hispanic": "0.039", "swing": true, "ECP": "0.74%"}, {"name": "New Jersey", "tieP": "22.51696935", "white": "0.545", "black": "0.124", "hispanic": "0.21", "swing": false, "ECP": "2.60%"}, {"name": "New Mexico", "tieP": "95.38212224", "white": "0.367", "black": "0.017", "hispanic": "0.495", "swing": false, "ECP": "0.93%"}, {"name": "New York", "tieP": "10.28089407", "white": "0.552", "black": "0.14", "hispanic": "0.193", "swing": false, "ECP": "5.39%"}, {"name": "North Carolina", "tieP": "19.06925993", "white": "0.627", "black": "0.209", "hispanic": "0.097", "swing": true, "ECP": "2.79%"}, {"name": "North Dakota", "tieP": "262.4458377", "white": "0.841", "black": "0.024", "hispanic": "0.04", "swing": false, "ECP": "0.56%"}, {"name": "Ohio", "tieP": "17.10995714", "white": "0.786", "black": "0.122", "hispanic": "0.04", "swing": true, "ECP": "3.35%"}, {"name": "Oklahoma", "tieP": "50.54371134", "white": "0.65", "black": "0.069", "hispanic": "0.111", "swing": false, "ECP": "1.30%"}, {"name": "Oregon", "tieP": "47.41879354", "white": "0.75", "black": "0.018", "hispanic": "0.134", "swing": false, "ECP": "1.30%"}, {"name": "Pennsylvania", "tieP": "15.6225724", "white": "0.76", "black": "0.104", "hispanic": "0.078", "swing": true, "ECP": "3.72%"}, {"name": "Rhode Island", "tieP": "188.7930554", "white": "0.705", "black": "0.059", "hispanic": "0.166", "swing": false, "ECP": "0.74%"}, {"name": "South Carolina", "tieP": "38.84465131", "white": "0.637", "black": "0.262", "hispanic": "0.057", "swing": false, "ECP": "1.67%"}, {"name": "South Dakota", "tieP": "226.07581", "white": "0.819", "black": "0.022", "hispanic": "0.037", "swing": false, "ECP": "0.56%"}, {"name": "Tennessee", "tieP": "29.28611864", "white": "0.736", "black": "0.164", "hispanic": "0.057", "swing": false, "ECP": "2.04%"}, {"name": "Texas", "tieP": "6.897531411", "white": "0.411", "black": "0.117", "hispanic": "0.4", "swing": false, "ECP": "7.06%"}, {"name": "Utah", "tieP": "62.38384907", "white": "0.778", "black": "0.01", "hispanic": "0.144", "swing": false, "ECP": "1.12%"}, {"name": "Vermont", "tieP": "320.5184707", "white": "0.928", "black": "0.011", "hispanic": "0.018", "swing": false, "ECP": "0.56%"}, {"name": "Virginia", "tieP": "23.43149842", "white": "0.613", "black": "0.189", "hispanic": "0.096", "swing": true, "ECP": "2.42%"}, {"name": "Washington", "tieP": "26.26432177", "white": "0.675", "black": "0.038", "hispanic": "0.13", "swing": false, "ECP": "2.23%"}, {"name": "West Virginia", "tieP": "111.5979883", "white": "0.925", "black": "0.032", "hispanic": "0.014", "swing": false, "ECP": "0.93%"}, {"name": "Wisconsin", "tieP": "34.34989559", "white": "0.811", "black": "0.062", "hispanic": "0.071", "swing": true, "ECP": "1.86%"}, {"name": "Wyoming", "tieP": "345.5669804", "white": "0.838", "black": "0.01", "hispanic": "0.101", "swing": false, "ECP": "0.56%"}]
+
+let body = document.getElementsByTagName('body')[0];
+let holder = document.getElementById('switchHolder');
+let divArr = []
+holder.style.display = 'flex'
+holder.style.justifyContent = 'space-evenly'
+
+for(i=0; i<5; i++){
+    let div = document.createElement('div')
+    div.style.display = 'inline'
+    holder.appendChild(div)
+    divArr.push(div)
+}
+
+for(i=0; i<51; i++){
+    let toggl = document.createElement('label');
+    let input = document.createElement('input');
+    let txt = document.createElement('span');
+    txt.innerText = stateArr[i].name
+    txt.className = 'slider round';
+    input.type = 'checkbox'
+    input.className = 'input'
+    if(stateArr[i].swing) input.checked = true
+    toggl.className = 'switch';
+    toggl.style.display = 'block'
+    toggl.appendChild(input);
+    toggl.appendChild(txt);
+    if(i == 50){
+        holder.appendChild(toggl)
+        break
+    }
+    divArr[Math.floor(i / 10)].appendChild(toggl);
+    holder.appendChild(divArr[Math.floor(i / 10)])
+}
+
+inputArr = document.getElementsByClassName('input')
+
+
+function stateCalc(){
+    let sum = [0, 0, 0]
+    for(i=0; i<stateArr.length; i++){
+        if(inputArr[i].checked == true){
+            let state = stateArr[i]
+            sum[0] += parseFloat(state.white) * (parseFloat(state.tieP) * parseFloat(state.ECP))
+            sum[1] += parseFloat(state.black) * (parseFloat(state.tieP) * parseFloat(state.ECP))
+            sum[2] += parseFloat(state.hispanic) * (parseFloat(state.tieP) * parseFloat(state.ECP))
+        }
+    }
+    let total = sum[0] + sum[1] + sum[2];
+    let final = [sum[0]/total, sum[1]/total, sum[2]/total]
+    
+    let ctx = document.getElementById('myChart');
+
+    let data = {
+        labels: ["White", "Black", "Hispanic"],
+        datasets: [{
+            label: 'Custom',
+            backgroundColor: 'blue',
+            data: final
+        },
+        {
+            label: 'Current Electoral College',
+            backgroundColor: 'red',
+            data: [0.7628942717440774, 0.09808394017007073, 0.1390217880858518]
+        },
+        {
+            label: 'US Demographics',
+            backgroundColor: 'green',
+            data: [0.601, 0.122, 0.185]
+        }
+    ]
+    }
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        max: 1
+                    }
+                }]
+            }
+        }
+    });
+}
+
+for(i=0; i<inputArr.length; i++){
+    inputArr[i].addEventListener('change', stateCalc)
+}
+stateCalc()
+
